48d198a699fd48b875542ac84d28aebf89834375
Si Yujun <siuj0810@gmail.com>
2024-12-01 05:42:46 +0900

    전체적인 가벼운 UI 완성

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_mask_mvvm/model/store.dart';
 import 'package:http/http.dart' as http;
 
 void main() {
@@ -31,33 +32,136 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  Future fetch() async {
+  final List<Store> stores = [];
+  var isLoading = true;
+
+  Future<void> fetch() async {
+    setState(() {
+      isLoading = true;
+    });
+
     var url =
         'https://gist.githubusercontent.com/junsuk5/bb7485d5f70974deee920b8f0cd1e2f0/raw/063f64d9b343120c2cb01a6555cf9b38761b1d94/sample.json?lat=37.266389&lng=126.999333&m=5000';
 
     var response = await http.get(Uri.parse(url));
-    print('Response status: ${response.statusCode}');
-    print('Response body: ${jsonDecode(response.body)}');
+    final jsonResult = jsonDecode(response.body);
+
+    final jsonStores = jsonResult['stores'];
+
+    setState(() {
+      stores.clear();
+      jsonStores.forEach((e) {
+        stores.add(Store.fromJson(e));
+        isLoading = false;
+      });
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetch();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('title'),
+        title: Text(
+            '마스크 재고 있는 곳 : ${stores.where((e) => e.remainStat == 'plenty' || e.remainStat == 'some' || e.remainStat == 'few').length}곳'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              fetch();
+            },
+            icon: const Icon(
+              Icons.refresh,
+              color: Colors.white,
+            ),
+          ),
+        ],
         titleTextStyle: const TextStyle(
           color: Colors.white,
           fontSize: 24,
         ),
         backgroundColor: Colors.blue,
       ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            fetch();
-          },
-          child: const Text('테스트'),
+      body: isLoading
+          ? loadingWidget()
+          : ListView(
+              children: stores
+                  .where((e) =>
+                      e.remainStat == 'plenty' ||
+                      e.remainStat == 'some' ||
+                      e.remainStat == 'few')
+                  .map(
+                (e) {
+                  return ListTile(
+                    title: Text(e.name!),
+                    subtitle: Text(e.addr!),
+                    trailing: _buildRemainStatWidget(e),
+                  );
+                },
+              ).toList(),
+            ),
+    );
+  }
+
+  Widget _buildRemainStatWidget(Store store) {
+    var remainStat = '판매중지';
+    var description = '';
+    var color = Colors.black;
+
+    switch (store.remainStat) {
+      case 'plenty':
+        remainStat = '충분';
+        description = '100개 이상';
+        color = Colors.green;
+        break;
+      case 'some':
+        remainStat = '여유';
+        description = '30개 이상';
+        color = Colors.yellow;
+        break;
+      case 'few':
+        remainStat = '부족';
+        description = '30개 미만';
+        color = Colors.red;
+        break;
+      case 'empty':
+        remainStat = '소진임박';
+        description = '1개 이하';
+        color = Colors.grey;
+        break;
+      default:
+    }
+
+    return Column(
+      children: [
+        Text(
+          remainStat,
+          style: TextStyle(
+            color: color,
+            fontWeight: FontWeight.bold,
+            fontSize: 14,
+          ),
         ),
+        Text(
+          description,
+          style: TextStyle(color: color),
+        ),
+      ],
+    );
+  }
+
+  Widget loadingWidget() {
+    return const Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('정보를 가져오는 중'),
+          CircularProgressIndicator(),
+        ],
       ),
     );
   }
